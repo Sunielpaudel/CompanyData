551d955ab89bae55a6fb74708633a82143a9a921
Suniel <Suniel.paudel2047@gmail.com>
2023-05-15 17:09:43 -0700

    changed end points

--- src/main/java/com/example/suniel/companyproject/controller/AddressController.java
+++ src/main/java/com/example/suniel/companyproject/controller/AddressController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/addresses1")
+@RequestMapping("/addresses2")
 public class AddressController {
     @Autowired
     private AddressService addressService;
