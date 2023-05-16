c8f4965e73b66b5aada8c955771ba1fec215b96f
Suniel <Suniel.paudel2047@gmail.com>
2023-05-16 10:57:17 -0700

    Changed AddressController path variable

--- src/main/java/com/example/suniel/companyproject/controller/AddressController.java
+++ src/main/java/com/example/suniel/companyproject/controller/AddressController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/addresses2")
+@RequestMapping("/addresses21")
 public class AddressController {
     @Autowired
     private AddressService addressService;
